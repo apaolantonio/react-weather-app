67511daf5501999c2e3d35db0f83e39fc7e854e4
Agustin Paolantonio <agustinpaolantonio@gmail.com>
2022-07-15 22:31:44 -0300

    Tutorial React - Weather App

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const getApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0ccfdaacd1msh40fbc00f0c3f2bfp1035b7jsnb1e3bc2ac842",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1730a49661ff58fe84c5a60be521ccf3";
